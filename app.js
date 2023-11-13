dd6515f43e76d8fa8264a42ecc4f8edf04b03a87
yassine299 <yassinehajjoubi299@gmail.com>
2023-11-13 21:07:22 +0100

    update the code

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-//packages
 const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
+
 const app = express();
 
-//home
-app.get("/", (req, res) => {
-    res.send("hello")
-})
 
+//this some categorieNames 
+const categorieNames = [
+    "/electronique/",
+    "/telephone-tablette/",
+    "/ordinateurs-accessoires-informatique/",
+    "/maison-cuisine-jardin/",
+    "/fashion-mode/",
+    "/beaute-hygiene-sante/"
+];
 
-// get the products by categorie
-app.get("/categorie", async (req, res) => {
-    const baseUrl = "https://www.jumia.ma/ar/ordinateurs-pc/" + "?page=";
+// Fetch products by category
+app.get("/categorie/:categoryIndex", async (req, res) => {
+    const categoryIndex = parseInt(req.params.categoryIndex);
+    if (isNaN(categoryIndex) || categoryIndex < 0 || categoryIndex >= categorieNames.length) {
+        return res.status(400).send("Invalid category index");
+    }
+
+    const baseUrl = "https://www.jumia.ma" + categorieNames[categoryIndex] + "?page=";
     let page = 1;
     const products = [];
 
@@ -21,55 +31,74 @@ app.get("/categorie", async (req, res) => {
             const url = baseUrl + page;
             const { data } = await axios.get(url);
             const $ = cheerio.load(data);
-            const items = $("a.core");
-            if (items.length === 0) {
+            const productLinks = $("a.core");
+
+            if (productLinks.length === 0) {
                 break;
             }
-            for (const item of items) {
-                const link = "https://www.jumia.ma/" + $(item).attr("href");
-                const image = $(item).find("*").children().attr("data-src");
-                const name = $(item).find("h3.name").text().trim();
-                const priceText = $(item).find(".prc").text().trim().replace(/,/g, "");
+
+            for (const linkElement of productLinks) {
+                const link = "https://www.jumia.ma/" + $(linkElement).attr("href");
+                const image = $(linkElement).find("*").children().attr("data-src");
+                const name = $(linkElement).find("h3.name").text().trim();
+                const priceText = $(linkElement).find(".prc").text().trim().replace(/,/g, "");
                 const price = parseFloat(priceText);
-                if (isNaN(price)) {
-                    continue;
+
+                if (!isNaN(price)) {
+                    products.push({ name, price, link, image });
                 }
-                products.push({ name, price, link, image });
             }
             page++;
         }
         res.send(products);
     } catch (error) {
-        console.error(error);
-        res.status(500).send("An error occurred while fetching the data.");
+        console.error("Error fetching products:", error);
+        res.status(500).send("Error fetching products: " + error.message);
     }
 });
 
-// get Product Info
+
+
+//get product info by putting product link
 app.get("/product", async (req, res) => {
-    const url = "https://www.jumia.ma/ar/cappy-pack-cappy-pulpy-orange-1l8-34196896.html";
+    const url = "https://www.jumia.ma/xiaomi-10000mah-mi-18w-fast-charge-power-bank-3-black-48996584.html";
+
     try {
         const { data } = await axios.get(url);
         const $ = cheerio.load(data);
         const productInfo = $(".col10");
-
         const productImg = $("#imgs");
-        const productName = $(productInfo).find("h1").text().trim();
-        const productPrice = parseInt($(productInfo).find("span").first().text().replace(/,/g, "").trim());
-        const productImgUrl = $(productImg).find("*").children().attr("data-src").trim();
-        const productDescription = $("#jm > main > div:nth-child(2) > div.col12 > div.card.aim.-mtm > div.markup.-mhm.-pvl.-oxa.-sc").text().trim();
-        const rating = $("#jm > main > div:nth-child(1) > section > div > div.col10 > div.-phs > div.-df.-i-ctr.-pvxs > div").text();
-       
-        const product = { name: productName, price: productPrice, link: url, img: productImgUrl, productDescription: productDescription, rating: rating };
+
+        const productNameElement = $(productInfo).find("h1");
+        const productPriceElement = $(productInfo).find("span").first();
+        const productImgElement = $(productImg).find("*").children();
+        const productDescriptionElement = $("#jm > main > div:nth-child(2) > div.col12 > div.card.aim.-mtm > div.markup.-mhm.-pvl.-oxa.-sc");
+        const ratingElement = $("#jm > main > div:nth-child(1) > section > div > div.col10 > div.-phs > div.-df.-i-ctr.-pvxs > div");
+
+        const productName = productNameElement.length ? productNameElement.text().trim() : "No name found";
+        const productPrice = productPriceElement.length ? parseInt(productPriceElement.text().replace(/,/g, "").trim()) : 0;
+        const productImgUrl = productImgElement.length ? productImgElement.attr("data-src").trim() : "No image found";
+        const productDescription = productDescriptionElement.length ? productDescriptionElement.text().trim() : "No description found";
+        const rating = ratingElement.length ? ratingElement.text().trim() : "No rating found";
+
+        const product = {
+            name: productName,
+            price: productPrice,
+            link: url,
+            img: productImgUrl,
+            productDescription: productDescription,
+            rating: rating
+        };
         res.send(product);
     } catch (error) {
-        console.error(error);
-        res.status(500).send("An error occurred while fetching the data.");
+        console.error("Error fetching product info:", error);
+        res.status(500).send("Error fetching product info: " + error.message);
     }
 });
 
 
-//start the server
+
+
 app.listen(4000, () => {
-    console.log("server is up runing port :4000 ")
-})
+    console.log("Server is running on port: 4000");
+});
